4169990a989c267fcd7e4b55d6e16383b389a325
Siarhei Hlitsuk <moonspel98@gmail.com>
2022-02-09 11:48:42 +0300

    Dev: Lesson2 code added

---
+++ Appointment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace les2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person user = new Person();
+            user.OutputNames();
+        }
+    }
+}
