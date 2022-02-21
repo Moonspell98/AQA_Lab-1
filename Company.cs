b9eba4a535edaac76f2e6d24f5e51987ff7daa86
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-02-21 19:00:03 +0300

    T3: Candidate, Employee and Company classes added

---
+++ Company.cs
@@ -0,0 +1,11 @@
+﻿namespace CandidatesAndEmployees
+{
+    public class Company
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+
+    }
+}
