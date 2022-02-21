d256ff0b0396c75399af68895dbba95ac2883a18
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-02-21 20:50:45 +0300

    T3: Persons generators added

---
+++ EmployeesComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CandidatesAndEmployees
+{
+    public class EmployeesComparer : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(null, y)) return 1;
+            if (ReferenceEquals(null, x)) return -1;
+            var companyNameComparison = string.Compare(x.Company.Name, y.Company.Name, StringComparison.Ordinal);
+            if (companyNameComparison != 0) return companyNameComparison;
+            return x.Salary.CompareTo(y.Salary);
+        }
+    }
+}
