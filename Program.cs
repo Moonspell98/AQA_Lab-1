ba6709af9d6783ea9e753777088c25357e9a3c7a
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-02-21 18:56:25 +0300

    T3: Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CandidatesAndEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
