bbe824d0ef0ec8ed709425176a22a83ef189fdbd
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-04-04 19:03:23 +0200

    T4: RandomUtils class added

--- CandidatesAndEmployees/RandomPersonsGenerator.cs
+++ CandidatesAndEmployees/RandomPersonsGenerator.cs
@@ -8,11 +8,9 @@ namespace CandidatesAndEmployees
     {
         public static List<Candidate> GenerateCandidates()
         {
-            Random random = new Random();
-            var maxValue = random.Next(1, 100);
             var candidate = new Candidate();
             var candidates = new List<Candidate>();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < RandomUtils.MaxPersonsCount(); i++)
             {
                 candidates.Add((Candidate)UserFactory.GenerateUser(candidate));
             }
@@ -21,11 +19,9 @@ namespace CandidatesAndEmployees
 
         public static List<Employee> GenerateEmployees()
         {
-            Random random = new Random();
-            var maxValue = random.Next(1, 100);
             var employee = new Employee();
             var employees = new List<Employee>();
-            for (int i = 0; i < maxValue; i++)
+            for (int i = 0; i < RandomUtils.MaxPersonsCount(); i++)
             {
                 employees.Add((Employee)UserFactory.GenerateUser(employee));
             }
---
+++ CandidatesAndEmployees/RandomUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CandidatesAndEmployees
+{
+    public class RandomUtils
+    {
+        public static int MaxPersonsCount()
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+    }
+}
