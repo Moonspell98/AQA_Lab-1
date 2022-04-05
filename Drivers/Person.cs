a9147180a08d5e8c1f3fc7040647b830aaf961d1
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-04-05 18:27:50 +0200

    T3: Fake person generator created

--- Drivers/Person.cs
+++ Drivers/Person.cs
@@ -1,11 +1,47 @@
-﻿namespace Person;
+﻿using Bogus.DataSets;
+
+namespace Person;
 
 public class Person
 {
     public string Surname { get; set; }
     public string Name { get; set; }
-    public DateOnly DateOfBirth { get; set; }
-    public DateOnly DateOfGetLicense { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public bool IsDriver
+    { 
+        get
+             {
+                 return IsDriver;
+             }
+        set
+        {
+            if ((DateTime.Now - DateOfBirth).Days >= 5840)
+            {
+                IsDriver = value;
+            }
+            else
+            {
+                IsDriver = false;
+            }
+        }
+    }
+
+    public DateTime? DateOfGetLicense
+    {
+        get { return DateOfGetLicense; }
+        set
+        {
+            if (IsDriver)
+            {
+                DateOfGetLicense = value;
+            }
+            else
+            {
+                DateOfGetLicense = null;
+            }
+        }
+    }
     public Guid LicenseId = new Guid();
-    public bool IsDriver;
+
 }
---
+++ Drivers/PersonGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+
+namespace Person;
+
+public class PersonGenerator
+{
+    public static Person CreatePerson()
+    {
+        var fakePerson = new Faker<Person>()
+            .RuleFor(p => p.Surname, f => f.Name.LastName())
+            .RuleFor(p => p.Name, f => f.Name.FirstName())
+            .RuleFor(p => p.DateOfBirth, f => f.Date.Past())
+            .RuleFor(p => p.IsDriver, f => f.Random.Bool())
+            .RuleFor(p => p.DateOfGetLicense, (f, p) => f.Date.Future(16, p.DateOfBirth))
+            .RuleFor(p => p.LicenseId, f => f.Random.Guid());
+        return fakePerson.Generate();
+    }
+}
