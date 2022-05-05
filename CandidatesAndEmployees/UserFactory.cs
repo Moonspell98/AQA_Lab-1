6d40b3f824c6e3cf83bb20b42a520318a8b21e4e
Siarhei Hlitsuk <Siarhei.Hlitsuk@itechart-group.com>
2022-05-05 18:51:34 +0200

    T4: CreateCompany added in Employee faker

--- CandidatesAndEmployees/UserFactory.cs
+++ CandidatesAndEmployees/UserFactory.cs
@@ -38,15 +38,19 @@ namespace CandidatesAndEmployees
                 .RuleFor(em => em.Position, f => f.Name.JobTitle())
                 .RuleFor(em => em.PositionDescription, f => f.Name.JobDescriptor())
                 .RuleFor(em => em.id, f => f.Random.Guid())
-                .RuleFor(em => em.Salary, f => f.Random.Int(500, 5000));
+                .RuleFor(em => em.Salary, f => f.Random.Int(500, 5000))
+                .RuleFor(em => em.Company, f => CreateCompany());
+            return fakeEmployee.Generate();
+        }
+
+        private static Company CreateCompany()
+        {
             var fakeCompany = new Faker<Company>()
                 .RuleFor(cp => cp.Name, f => f.Company.CompanyName())
                 .RuleFor(cp => cp.Address, f => f.Address.StreetAddress())
                 .RuleFor(cp => cp.City, f => f.Address.City())
                 .RuleFor(cp => cp.Country, f => f.Address.Country());
-            Employee employee = fakeEmployee.Generate();
-            employee.Company = fakeCompany.Generate();
-            return employee;
+            return fakeCompany.Generate();
         }
     }
 }
